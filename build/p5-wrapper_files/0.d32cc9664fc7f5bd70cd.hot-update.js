bcf05845c83fa8ff24c24420b5b70ed30c9eb586
mark sauer-utley <mark.utley.713@gmail.com>
2018-09-12 17:23:48 -0400

    ready to deploy

---
+++ build/p5-wrapper_files/0.d32cc9664fc7f5bd70cd.hot-update.js
@@ -0,0 +1,24 @@
+webpackHotUpdate(0,{
+
+/***/ "./node_modules/css-loader/index.js?{\"importLoaders\":1}!./node_modules/postcss-loader/lib/index.js?{\"ident\":\"postcss\",\"plugins\":[null,null]}!./src/Home.css":
+/*!************************************************************************************************************************************************!*\
+  !*** ./node_modules/css-loader?{"importLoaders":1}!./node_modules/postcss-loader/lib?{"ident":"postcss","plugins":[null,null]}!./src/Home.css ***!
+  \************************************************************************************************************************************************/
+/*! dynamic exports provided */
+/*! all exports used */
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__(/*! ../node_modules/css-loader/lib/css-base.js */ "./node_modules/css-loader/lib/css-base.js")(undefined);
+// imports
+
+
+// module
+exports.push([module.i, "/* html, .homeBody {\nwidth: 100%;\nheight: 100%;\npadding: 0;\nmargin: 0;\nbackground-image: radial-gradient(circle at center, #27588e 0%, #103359 25%, #223347 60%, #131a22 85% );\nfont-size: 0;\n} */\n\nsvg{\ndisplay: block;\nwidth: 100%;\nheight: 100%;\npadding: 0;\nmargin: 0;\nposition:absolute;\n\n}\n\nh1{\nwidth: 100%;\nfont-family: sans-serif;\nposition:absolute;\ntext-align:center;\ncolor:white;\nfont-size: 18px;\ntop: 40%;\nopacity: 1;\n-o-transition: opacity 1s ease-in-out;\ntransition: opacity 1s ease-in-out;\n-moz-transition: opacity 1s ease-in-out;\n-webkit-transition: opacity 1s ease-in-out;\n}\n\nh1 a{\n  color: #000;\n  text-decoration:none;\n}\n\npath{\nstroke-linecap: square;\nstroke: white;\nstroke-width: 0.5px;\n}\n\n\n.col-centered{\n    float: none;\n    margin: 0 auto;\n}\n\n.container {\n\tmargin-left: auto;\n\tmargin-right: auto;\n\tmax-width: 720px;\n\tpadding: 1em;\n}\n\n.search-result {\n  font-style: italic;\n  font-size: 0.8em;\n  padding: 2px;\n  color: white;\n  background-color: blue;\n  overflow: hidden;\n}\n\n.search-result:hover {\n  font-style: normal;\n  background-color: purple;\n}\n\n.search-result p {\n  margin: 8px;\n  display: -ms-inline-flexbox;\n  display: inline-flex;\n}\n\n.search-result button {\n  margin: 5px;\n  float: left;\n  display: -ms-inline-flexbox;\n  display: inline-flex;\n  background-color: black;\n  color: white;\n}\n\n.search-result button:hover {\n  background-color: purple;\n}\n\n.scroll-left {\n  height: 35px;\n  position: relative;\n}\n\n.scroll-left p {\n  margin: -7px 0px 0px 8px;\n  position: absolute;\n  width: 100%;\n  height: 100%;\n  line-height: 50px;\n  text-align: left;\n}\n\n.scroll-left button {\n  z-index: 10000;\n}\n\n.scroll-left:hover p {\n  /* Starting position */\n  -webkit-transform:translateX(50%);\n  -ms-transform:translateX(50%);\n      transform:translateX(50%);\n  /* Apply animation to this element */\n  -webkit-animation: scroll-left 10s linear infinite;\n  animation: scroll-left 10s linear infinite;\n}\n/* Move it (define the animation) */\n\n@-webkit-keyframes scroll-left {\n  0% { -webkit-transform: translateX(100%); }\n  100% { -webkit-transform: translateX(-100%); }\n}\n\n@keyframes scroll-left {\n  0% { /* Browser bug fix */\n    -webkit-transform: translateX(100%); /* Browser bug fix */\n    transform: translateX(100%);\n  }\n  100% { /* Browser bug fix */\n    -webkit-transform: translateX(-100%); /* Browser bug fix */\n    transform: translateX(-100%);\n  }\n}\n", ""]);
+
+// exports
+
+
+/***/ })
+
+})
+//# sourceMappingURL=0.d32cc9664fc7f5bd70cd.hot-update.js.map
